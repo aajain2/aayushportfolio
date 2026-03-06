a97b4a7655cf91071773f5d986a4f81d28e5ef2d
Aayush Jain <118139337+aajain2@users.noreply.github.com>
2026-03-05 17:34:51 -0800

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   server: {
     open: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        contact: resolve(__dirname, 'contact.html'),
+      }
+    }
   }
 })
